909264094dc992f0178f64476974451e27a1af18
Michael Castro Montaño <86693441+michaelcm06@users.noreply.github.com>
2023-11-20 20:02:05 -0500

    Delete src directory

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt'
-
-
-export const generaHash=(password)=>bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-
-export const validaHash=(usuario, password)=>bcrypt.compareSync(password, usuario.password)
